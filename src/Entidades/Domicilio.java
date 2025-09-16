09cff280cf5c9b19060ea49ec20c561f4f2879d0
EzequielAbadie <abadieezequiel@gmail.com>
2025-09-15 22:28:49 -0300

    pais

--- src/Entidades/Domicilio.java
+++ src/Entidades/Domicilio.java
@@ -1,4 +1,18 @@
 package Entidades;
 
+import lombok.*;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@ToString(exclude = "sucursal")
+
 public class Domicilio {
+    private Long id;
+    private String calle;
+    private Integer numero;
+    private Integer codigoPostal;
+    private Localidad localidad;
 }
--- src/Entidades/Pais.java
+++ src/Entidades/Pais.java
@@ -1,15 +1,21 @@
 package Entidades;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
+@ToString
 
 
 public class Pais {
     private long id;
     private String nombre;
 
-
+    @Builder.Default
+    private Set<Provincia> provincias = new HashSet<>();
 }
